823b0fd87d6e668f2e742d5010101509318ace1a
riyadhvirtual <riyadhvirtual@gmail.com>
2025-10-14 19:09:33 +0530

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   out: './migrations',
   schema: './db/schema.ts',
-  dialect: 'pg',
+  dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
